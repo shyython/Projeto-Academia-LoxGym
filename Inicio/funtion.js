72b3090b422c1ad950eab02b7ede02924912619f
shyython <beatriz.follow@gmail.com>
2025-04-19 03:00:55 -0300

    .

---
+++ Inicio/funtion.js
@@ -0,0 +1,15 @@
+function clickmenu() {
+    if (itens.style.display == "block") {
+        itens.style.display = "none"
+    } else {
+        itens.style.display = "block"
+    }
+}
+
+function mudouTamanho(){
+    if (window.innerWidth >= 768){
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
