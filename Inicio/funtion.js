a63ba0aa064840a2f2e01eec68dd0e9d54123883
shyython <beatriz.follow@gmail.com>
2025-04-21 00:52:41 -0300

    .

--- Inicio/funtion.js
+++ Inicio/funtion.js
@@ -1,17 +1,21 @@
- function clickMenu() {
+function clickMenu() {
     if (itens.style.display == "block") {
-       itens.style.display = "none"
-   } else {
-       itens.style.display = "block"
-       
-        
-   }
- }
+        itens.style.display = "none"
+    }
 
- function mudouTamanho(){
-     if (window.innerWidth >= 768){
-         itens.style.display = 'block'
-     } else {
-         itens.style.display = 'none'
-     }
- }
+    else {
+        itens.style.display = "block"
+
+    }
+
+    //aplicar a mesma logica de cima porem ao contrario
+    //fazer a logo e os botoes sumirem quando os itens aparecerem
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
